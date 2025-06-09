a629b8e0701947ac9bf02e45583c83db70dbc86c
Danny Saban <danny@illumex.ai>
2025-06-09 18:58:28 +0300

    fix requirements2.txt

--- to_git.sh
+++ to_git.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git add .
-git commit -m "fix requirements.txt"
+git commit -m "fix requirements2.txt"
 git remote add origin https://github.com/dsaban/demo_dash_warl0k.git
 git push -u origin main
 
