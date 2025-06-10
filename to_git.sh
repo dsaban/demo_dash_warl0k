1302fd9e090f88706a942034c8074ca988a3fb17
Danny Saban <danny@illumex.ai>
2025-06-10 21:20:11 +0300

    numpy==1.26.4

--- to_git.sh
+++ to_git.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git add .
-git commit -m "change new dash app - add new bar for noise, log and config file"
+git commit -m "numpy==1.26.4"
 git remote add https://github.com/dsaban/demo_dash_warl0k.git
 git push -u
 
