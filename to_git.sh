79e25be7842691ac9f9b7fbe5f82c50b14dbbfb3
Danny Saban <danny@illumex.ai>
2025-06-21 14:43:54 +0300

    add warl0k_dashboard_new.py

--- to_git.sh
+++ to_git.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 git add .
-git commit -m "clean code: demo_warl0k_dash_root_main.py"
+git commit -m "add warl0k_dashboard_new.py"
 git remote add https://github.com/dsaban/demo_dash_warl0k.git
-git push -u
+#git push <name> <branch>
+git push -u origin main
 
